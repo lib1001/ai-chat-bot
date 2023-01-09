26f0e2b441131ae4b7fe41fda05bfa2c9b39d2ba
Lauren <lburke522@gmail.com>
2023-01-09 11:54:29 -0500

    added function to type dots while bot is loading/thinking

---
+++ client/main.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('form');
+const chat = document.querySelector('#chat-container');
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = "";
+
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
+
+    if (element.textContent === "....") {
+      element.textContent = "";
+    }
+  }, 300);
+}
