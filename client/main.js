949b06154cd6335f9cba6f9ebfa05a60e787b7b9
Lauren <lburke522@gmail.com>
2023-01-09 11:58:04 -0500

    added function for random id generator

--- client/main.js
+++ client/main.js
@@ -13,4 +13,25 @@ function loader(element) {
       element.textContent = "";
     }
   }, 300);
-}
+};
+
+function typeText(element, text) {
+  let i = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.textContent += text.charAt(i);
+      i++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+};
+
+function generateId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+};
