8986318d502539a2f01877b727dae1cedc9885ce
Lauren <lburke522@gmail.com>
2023-01-09 12:02:24 -0500

    handlesubmit function for form submit

--- client/main.js
+++ client/main.js
@@ -36,15 +36,34 @@ function generateId() {
   return `id-${timestamp}-${hexadecimalString}`;
 };
 
-function chatStripe(isAi, value, uniqueId) {
+function chatArea(isAi, value, id) {
   return `
     <div class='wrapper' ${isAi && "ai"}>
       <div class='chat'>
         <div classe='profile'>
           <img src="${isAi ? bot : user}" alt=${isAi ? "bot" : "user"} />
         </div>
-        <div class='message' id=${uniqueId}>${value}>
+        <div class='message' id=${id}>${value}>
         </div>
       </div>
     </div>`;
+};
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  chat.innerHTML += chatArea(false, data.get("prompt"));
+
+  form.reset();
+
+  const id = generateId();
+  chat.innerHTML += chatArea(true, " ", id);
+
+  chat.scrollTop = chat.scrollHeight;
+
+  const message = document.getElementById(id);
+
+  loader(message);
 }
