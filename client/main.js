a87c38cd8d291b529c95c0c93c85acaf766e48a8
Lauren <lburke522@gmail.com>
2023-01-09 14:19:16 -0500

    updated server url after deploying server

--- client/main.js
+++ client/main.js
@@ -70,7 +70,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:3000', {
+  const response = await fetch('https://ai-chat-2vu9.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
