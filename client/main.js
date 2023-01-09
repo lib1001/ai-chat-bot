cf4e1c819c204fb5fe5eb43cb855b45e5b6792bd
Lauren <lburke522@gmail.com>
2023-01-09 13:56:59 -0500

    a few touchups, will go back and edit styling

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import bot from './assets/bot.png';
 import user from './assets/user.png';
 
 const form = document.querySelector("form");
-const chatContainer = document.querySelector("#chat-container");
+const chat = document.querySelector("#chat-container");
 
 let loadInteral;
 
@@ -57,14 +57,14 @@ const handleSubmit = async (e) => {
 
   const data = new FormData(form);
 
-  chatContainer.innerHTML += chatArea(false, data.get("prompt"));
+  chat.innerHTML += chatArea(false, data.get("prompt"));
 
   form.reset();
 
   const id = generateId();
-  chatContainer.innerHTML += chatArea(true, " ", id);
+  chat.innerHTML += chatArea(true, " ", id);
 
-  chatContainer.scrollTop = chatContainer.scrollHeight;
+  chat.scrollTop = chat.scrollHeight;
 
   const messageDiv = document.getElementById(id);
 
@@ -103,103 +103,3 @@ form.addEventListener("keyup", (e) => {
     handleSubmit(e);
   }
 });
-
-
-// const form = document.querySelector('form');
-// const chat = document.querySelector('#chat-container');
-
-// let loadInterval;
-
-// function loader(element) {
-//   element.textContent = "";
-
-//   loadInterval = setInterval(() => {
-//     element.textContent += ".";
-
-//     if (element.textContent === "....") {
-//       element.textContent = "";
-//     }
-//   }, 300);
-// };
-
-// function typeText(element, text) {
-//   let i = 0;
-
-//   let interval = setInterval(() => {
-//     if (i < text.length) {
-//       element.textContent += text.charAt(i);
-//       i++;
-//     } else {
-//       clearInterval(interval);
-//     }
-//   }, 20);
-// };
-
-// function generateId() {
-//   const timestamp = Date.now();
-//   const randomNumber = Math.random();
-//   const hexadecimalString = randomNumber.toString(16);
-
-//   return `id-${timestamp}-${hexadecimalString}`;
-// };
-
-// function chatArea(isAi, value, id) {
-//   return `
-//     <div class='wrapper' ${isAi && "ai"}>
-//       <div class='chat'>
-//         <div classe='profile'>
-//           <img src="${isAi ? bot : user}" alt=${isAi ? "bot" : "user"} />
-//         </div>
-//         <div class='message' id=${id}>${value}
-//         </div>
-//       </div>
-//     </div>`;
-// };
-
-// const handleSubmit = async (e) => {
-//   e.preventDefault();
-
-//   const data = new FormData(form);
-
-//   chat.innerHTML += chatArea(false, data.get("prompt"));
-
-//   form.reset();
-
-//   const id = generateId();
-//   chat.innerHTML += chatArea(true, " ", id);
-
-//   chat.scrollTop = chat.scrollHeight;
-
-//   const message = document.getElementById(id);
-
-//   loader(message);
-
-//   const response = await fetch('http://localhost:3000', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({
-//       prompt: data.get('prompt')
-//     })
-//   })
-
-//   clearInterval(loadInterval);
-//   message.innerHTML = "";
-
-//   if(response.ok) {
-//     const data = await response.json();
-//     const parsedData = data.bot.trim();
-//   } else {
-//     const err = await response.text();
-//     message.innerHTML = "Uh oh! Something's wrong!";
-
-//     alert(err);
-//   }
-//   }
-
-// form.addEventListener("submit", handleSubmit);
-// form.addEventListener('keyup', (e) => {
-//   if (e.keyCode === 13) {
-//     handleSubmit(e);
-//   }})
